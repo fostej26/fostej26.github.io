347879767bad389f14a3d624af7a758d53a55a70
Jacob Foster <fostej26@mcmaster.ca>
2024-07-30 19:31:22 -0700

    updates projects

--- src/App.js
+++ src/App.js
@@ -101,10 +101,11 @@ function App() {
         </div>
       <div>
         <section>
-          <div className='flex container'>
+          <div className='flex container' id="about">
             <div className='w-1/12'></div>
-            <div id="about" className="flex flex-col container w-11/12 justify-around">
+            <div className="flex flex-col container w-11/12 justify-around">
               <h2 className="text-6xl font-bold pb-6 ">About Me:</h2>
+              <hr className='border-2 border-neutral-300 w-full'></hr>
               <p className="text-2xl text-left mt-4 w-10/12 ">
                 Hi I'm Jacob. I just finished my second year of mechatronics engineering at McMaster University in Hamilton, ON and am taking a break from my studies to
                 pursue a co-op position at L3Harris Technologies as a manufacturing software development intern. Through my school and work experiences, I have discovered
@@ -122,10 +123,11 @@ function App() {
         <section>
         <div className='flex container mt-20'>
   <div className='w-1/12'></div>
-  <div id="experience" className="flex flex-col items-left w-8/12">
+  <div id="experience" className="flex flex-col items-left w-full">
     <h2 className="text-6xl mb-10 font-bold">Experience:</h2>
-    <div className="flex container items-center" id="experience-container">
-      <div className="bg-white p-10 pl-0 pt-0 pb-10 rounded-xl shadow-lg w-full">
+    <hr className='border-2 border-neutral-300 w-full'></hr>
+    <div className="grid grid-cols-2 grid-rows-1 gap-4 mt-4" id="experience-container">
+    <div className="p-10 pl-0 pt-0 pb-10 w-full border-2 border-neutral-300 rounded-lg">
         <a href="https://www.l3harris.com/">
           <img src={require("./assets/L3Harris_Technologies-Logo.png")} className="w-60 h-40" alt="L3Harris Technologies Logo" />
         </a>
@@ -154,17 +156,93 @@ function App() {
 </section>
 
 
-    <section>
-      <div className='flex flex-cols container'>
-        <div id="projects" className="grid grid-cols-2 grid-rows-2 gap-4">
-          <div class="bg-blue-500 h-24 flex items-center justify-center text-white">1</div>
-          <div class="bg-green-500 h-24 flex items-center justify-center text-white">2</div>
-          <div class="bg-red-500 h-24 flex items-center justify-center text-white">3</div>
-          <div class="bg-yellow-500 h-24 flex items-center justify-center text-white">4</div> 
-        </div>
-      </div>
+<section>
+  <div className='flex container mt-20'>
+    <div className='w-1/12'></div>
+    <div id="experience" className="flex flex-col items-start w-full">
+      <h2 className="text-6xl mb-10 font-bold">Projects:</h2>
+      <hr className='border-2 border-neutral-300 w-full'></hr>
+      <div className="grid grid-cols-3 grid-rows-2 gap-4 mt-4" id="projects-grid">
 
-    </section>
+        <div className="w-full border-2 border-neutral-300 rounded-lg p-10">
+          <div className='flex justify-center'>
+            <a href="https://github.com/fostej26/onTime/tree/main">
+              <img src={require("./assets/onTime.jpg")} className="w-48 h-48 rounded-lg" alt="onTime app graphic" />
+            </a>
+          </div>
+          <h3 className='text-center text-xl font-semibold mt-4'>onTime</h3>
+          <p className='text-md font-normal mt-2 text-center'>
+            A python program that provides users with an SMS containing the optimal time of departure for events logged
+            in their Google Calendar based on the current weather conditions and traffic data.
+          </p>
+        </div>
+
+        <div className="w-full border-2 border-neutral-300 rounded-lg p-10">
+          <div className='flex justify-center'>
+            <a href="https://github.com/fostej26/onTime/tree/main">
+              <img src={require("./assets/sumo.PNG")} className="w-48 h-48 rounded-lg" alt="onTime app graphic" />
+            </a>
+          </div>
+          <h3 className='text-center text-xl font-semibold mt-4'>Spinnick SumoBot</h3>
+          <p className='text-md font-normal mt-2 text-center'>
+            An automated robot designed to compete in the annual SumoBot competition at McMaster University. Perceives the environment using
+            ultrasonic and color sensors and was loaded with attack and evade algorithms. Group Champions 2024.
+          </p>
+        </div>
+
+        <div className="w-full border-2 border-neutral-300 rounded-lg p-10">
+          <div className='flex justify-center'>
+            <a href="https://github.com/fostej26/onTime/tree/main">
+              <img src={require("./assets/allergypal.jpg")} className="w-48 h-48 rounded-lg" alt="team photo from sumobot competition" />
+            </a>
+          </div>
+          <h3 className='text-center text-xl font-semibold mt-4'>AllergyPal</h3>
+          <p className='text-md font-normal mt-2 text-center'>
+            A web application that allows users input a list of food sensitivities and take photos of meals they are unsure about. Using
+            a machine learning model, the app will detect the dish and cross-reference the ingredients with the user's sensitivities.
+          </p>
+        </div>
+
+        <div className="w-full border-2 border-neutral-300 rounded-lg p-10">
+          <div className='flex justify-center'>
+            <a href="https://github.com/fostej26/fostej26.github.io">
+              <img src={require("./assets/website.PNG")} className="w-48 h-48 rounded-lg" alt="onTime app graphic" />
+            </a>
+          </div>
+          <h3 className='text-center text-xl font-semibold mt-4'>Portfolio Website</h3>
+          <p className='text-md font-normal mt-2 text-center pt-10'>
+            A react.js website used to display my resume, projects, and contact information.
+          </p>
+        </div>
+
+        <div className="w-full border-2 border-neutral-300 rounded-lg p-10">
+          <div className='flex justify-center'>
+            <a href="https://github.com/fostej26/onTime/tree/main">
+              <img src={require("./assets/matrix.jpg")} className="w-48 h-48 rounded-lg" alt="onTime app graphic" />
+            </a>
+          </div>
+          <h3 className='text-center text-xl font-semibold mt-4'>Sparce Matrix Solver</h3>
+          <p className='text-md font-normal mt-2 text-center'>
+            blah vlah blah do tmos.
+          </p>
+        </div>
+
+        <div className="w-full border-2 border-neutral-300 rounded-lg p-10">
+          <div className='flex justify-center'>
+            <a href="https://github.com/fostej26/onTime/tree/main">
+              <img src={require("./assets/allergypal.jpg")} className="w-48 h-48 rounded-lg" alt="team photo from sumobot competition" />
+            </a>
+          </div>
+          <h3 className='text-center text-xl font-semibold mt-4'>AllergyPal</h3>
+          <p className='text-md font-normal mt-2 text-center'>
+            dont even know whats gonna go here
+          </p>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</section>
 
 
 
