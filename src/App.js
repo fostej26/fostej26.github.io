fb50c070f1cdd3e278ade60d91f7b7bd4e3fa4f1
fostej26 <fostej26@mcmaster.ca>
2024-08-22 11:21:46 -0400

    undo

--- src/App.js
+++ src/App.js
@@ -108,7 +108,7 @@ function App() {
           </span>
         </h1>
         
-        <h3 id="typewriter" className="text-4xl font-medium h-auto text-left">
+        <h3 id="typewriter" className="text-4xl font-medium h-auto">
           <span>{text}</span>
           <span>
             <Cursor />
